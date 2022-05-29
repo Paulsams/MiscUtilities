3d116fd9e4547e2c956a8110e4add99e78497ec8
Paulsams <pasha.samsonov234@gmail.com>
2022-05-29 21:49:59 +0300

    I updated the folder name and pulled GameObjectLayer from my namespace.

---
+++ GameObjectLayer/Editor/GameObjectLayerDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(GameObjectLayer))]
+public class GameObjectLayerDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        var layerProperty = property.FindPropertyRelative("_layer");
+
+        layerProperty.intValue = EditorGUI.LayerField(position, layerProperty.intValue);
+
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ GameObjectLayer/Runtime/GameObjectLayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct GameObjectLayer
+{
+    [SerializeField] private int _layer;
+
+    public static implicit operator int(GameObjectLayer layer)
+    {
+        return layer._layer;
+    }
+
+    public static implicit operator GameObjectLayer(int x)
+    {
+        return new GameObjectLayer { _layer = x };
+    }
+}
