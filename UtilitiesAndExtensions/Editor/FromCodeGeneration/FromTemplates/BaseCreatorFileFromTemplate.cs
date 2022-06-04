1058fa486019f3c1bfc987ff81ec46b7d6806947
Paulsams <pasha.samsonov234@gmail.com>
2022-06-04 21:53:17 +0300

    Redesigned the base class for code generation on templates.

--- UtilitiesAndExtensions/Editor/FromCodeGeneration/FromTemplates/BaseCreatorFileFromTemplate.cs
+++ UtilitiesAndExtensions/Editor/FromCodeGeneration/FromTemplates/BaseCreatorFileFromTemplate.cs
@@ -2,26 +2,68 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEditor;
+using UnityEngine;
 
 namespace Paulsams.MicsUtils.CodeGeneration
 {
     public abstract class BaseCreatorScriptFromTemplate
     {
-        protected Dictionary<string, string> _keyToValues;
+        public struct FileCreateInfo
+        {
+            public readonly string TemplateScript;
+            public readonly string FileName;
+            public readonly string AbsolutePathToFolder;
+
+            public FileCreateInfo(string baseScript, string fileName, string absolutePathToFolder)
+            {
+                TemplateScript = baseScript;
+                FileName = fileName;
+                AbsolutePathToFolder = absolutePathToFolder;
+            }
+        }
+
+        protected abstract string TextError { get; }
+
+        protected Dictionary<string, string> _properties;
+
+        protected string this[string key]
+        {
+            set
+            {
+                _properties[key] = value;
+            }
+        }
 
         protected BaseCreatorScriptFromTemplate(string scriptNamespace)
         {
-            _keyToValues = new Dictionary<string, string>()
+            _properties = new Dictionary<string, string>()
             {
                 ["NamespaceBegin"] = $"namespace {scriptNamespace}\r\n{{",
                 ["NamespaceEnd"] = "}",
             };
         }
 
-        public abstract void Create();
-
-        protected void CreateFile(string baseScript, string fileName, string absolutePathToFolder)
+        public void Create()
         {
+            if (ValidCreate())
+            {
+                foreach (var info in OnSetPropertiesAndGetFileCreateInfos())
+                {
+                    CreateFile(info);
+                }
+            }
+            else
+            {
+                Debug.LogError(TextError);
+            }
+        }
+
+        protected abstract bool ValidCreate();
+        protected abstract IEnumerable<FileCreateInfo> OnSetPropertiesAndGetFileCreateInfos();
+
+        private void CreateFile(FileCreateInfo info)
+        {
+            var baseScript = info.TemplateScript;
             StringBuilder script = new StringBuilder((int)(baseScript.Length * 1.5f));
             bool isWord = false;
             StringBuilder word = new StringBuilder();
@@ -31,7 +73,7 @@ namespace Paulsams.MicsUtils.CodeGeneration
                 {
                     if (baseScript[i] == '#')
                     {
-                        if (_keyToValues.TryGetValue(word.ToString(), out string value))
+                        if (_properties.TryGetValue(word.ToString(), out string value))
                             script.Append(value);
 
                         word.Clear();
@@ -53,10 +95,11 @@ namespace Paulsams.MicsUtils.CodeGeneration
                 }
             }
 
+            var absolutePathToFolder = info.AbsolutePathToFolder;
             if (Directory.Exists(absolutePathToFolder) == false)
                 Directory.CreateDirectory(absolutePathToFolder);
 
-            File.WriteAllText($"{absolutePathToFolder}/{fileName}.cs", script.ToString());
+            File.WriteAllText($"{absolutePathToFolder}/{info.FileName}.cs", script.ToString());
             AssetDatabase.Refresh();
         }
     }
