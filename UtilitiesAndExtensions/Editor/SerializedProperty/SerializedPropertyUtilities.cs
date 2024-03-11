0016361031912bd05b43a3272bd368ce05d70dc7
Paulsams <pasha.samsonov234@gmail.com>
2024-03-12 00:44:09 +0300

    Fixed throwing exception in SerializedPropertyUtilities.GetManagedReferenceType

--- UtilitiesAndExtensions/Editor/SerializedProperty/SerializedPropertyUtilities.cs
+++ UtilitiesAndExtensions/Editor/SerializedProperty/SerializedPropertyUtilities.cs
@@ -9,6 +9,9 @@ namespace Paulsams.MicsUtils
         public static Type GetManagedReferenceType(string typename)
         {
             var splitFieldTypename = typename.Split(' ');
+            if (splitFieldTypename.Length != 2)
+                return null;
+            
             var assemblyName = splitFieldTypename[0];
             var typeName = splitFieldTypename[1];
             var assembly = Assembly.Load(assemblyName);
